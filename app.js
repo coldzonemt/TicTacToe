d05c8a0f2d40570bf2e3a4e01c82366c81464a96
Kristin <coldzonemt@yahoo.com>
2016-03-06 15:39:37 -0800

    angular refactor

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-//ng-App is "Game"
+var app = angular.module('TicTacToe', []); 
+
+app.controller('game', ['$scope', function($scope){
+
+	
+	$scope.rows = [0,1,2]; 
+	$scope.columns = [0,1,2]; 
+	
+
+	$scope.player1 = 'X'; 
+	$scope.player2 = 'Y'; 
+	$scope.winner = null; 
+	$scope.turn = 0; 
+
+	//every time there is a turn, we need to do the following: 
+	$scope.playTurn = function(){
+		$scope.checkWinner(); 
+		$scope.turn ++; 
+	}
+
+}])
+
