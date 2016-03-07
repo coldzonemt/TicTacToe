8ef4260eaca5a90571764d77ab6d9da3993a989c
Kristin <coldzonemt@yahoo.com>
2016-03-06 16:45:30 -0800

    build out implementation of playTurn function, includes testing of onClick functionality

--- app.js
+++ app.js
@@ -11,22 +11,43 @@ app.controller('game', ['$scope', function($scope){
 	$scope.winner = false; 
 	$scope.turn = 0; 
 
+	//Initialize a game (is this necessary if I have a 'global' $scope.turn variable?) 
+	playGame = function(){
+		//reset turns to 0
+		$scope.turn = 0; 
+		//reset board buttons all to "unclicked" state
+	}
+
 	//every time there is a turn, we need to do the following: 
 	$scope.playTurn = function(){
-		$scope.winner = $scope.checkWinner(); 
-		$scope.turn ++; 
+		var winner = checkWinner(); 
+		if($scope.turn %2 === 0) {
+			console.log('X');
+		} else {
+			console.log('O');
+		}
+
 		//if there is a winner
 		if(winner){
 			//check to see who just placed a letter (x or y?)
 
 			//return statement to excit function and declare winner
+		} 
+
+		//increment turn
+		$scope.turn ++;
+
+		if($scope.turn === 9) {
+			//alert players of a tie
+			console.log("game over, and it's a tie");
+			//create a new button that asks if user wants to play a new game 
+			playGame(); 
 		}
-		console.log($scope.turn); 
 	}
 
 	//returns a boolean if there is a winner or not
-	$scope.checkWinner = function(){
-
+	checkWinner = function(){
+		return false; 
 	}
 }]);
 
